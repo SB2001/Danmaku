8da90be43bd7de1e1bf7ff8492602cf8a2b5ecc6
Sarah Bailey <sabailey@sabailey-mn1.linkedin.biz>
2018-08-02 14:38:56 -0700

    fifth commit

--- danmaku.js
+++ danmaku.js
@@ -47,7 +47,6 @@ $(document).ready(function() {
     // pointer lock event listeners
 
     // Hook pointer lock state change events for different browsers
-    document.addEventListener("mousemove", updateHitbox, false)
     document.addEventListener('pointerlockchange', lockChangeAlert, false);
     document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
 
@@ -55,10 +54,10 @@ $(document).ready(function() {
       if (document.pointerLockElement === canvas ||
           document.mozPointerLockElement === canvas) {
         console.log('The pointer lock status is now locked');
-        document.addEventListener("mousemove", updatePosition, false);
+        document.addEventListener('mousemove', updatePosition, true)
       } else {
         console.log('The pointer lock status is now unlocked');
-        document.removeEventListener("mousemove", updatePosition, false);
+        document.removeEventListener('mousemove', updatePosition, true)
       }
     }
 
@@ -71,16 +70,17 @@ $(document).ready(function() {
       if (player.x > canvas.width + RADIUS) {
         player.x = canvas.width;
       }
-      if (y > canvas.height + RADIUS) {
+      if (player.y > canvas.height + RADIUS) {
         player.y = canvas.height;
       }
-      if (x < -RADIUS) {
+      if (player.x < -RADIUS) {
         player.x = RADIUS;
       }
-      if (y < -RADIUS) {
+      if (player.y < -RADIUS) {
         player.y = RADIUS;
       }
       tracker.textContent = "X position: " + player.x + ", Y position: " + player.y
+      getQuadrant(player.x, player.y);
 
     this.newPos = function() {
         this.player.x += this.speedX;
@@ -95,198 +95,93 @@ $(document).ready(function() {
         });
       }
     }
-    function QuadTree(boundBox, lvl) {
-        var maxObjects = 10;
-        this.bounds = boundBox || {
-            x: 0,
-            y: 0,
-            width: 0,
-            height: 0
-        };
-        this.nodes = [];
-        /*
-         * Clears the quadTree and all nodes of objects
-         */
-        this.clear = function() {
-            bullets = [];
-            for (var i = 0; i < this.nodes.length; i++) {
-                this.nodes[i].clear();
-            }
-            this.nodes = [];
-        };
-        /*
-         * Get all objects in the quadTree
-         */
-        this.getAllObjects = function(returnedObjects) {
-            for (var i = 0; i < this.nodes.length; i++) {
-                this.nodes[i].getAllObjects(returnedObjects);
-            }
-            for (var i = 0, len = bullets.length; i < len; i++) {
-                returnedObjects.push(bullets[i]);
-            }
-            return returnedObjects;
-        };
-        /*
-         * Return all objects that the object could collide with
-         */
-        this.findObjects = function(returnedObjects, bulletClone) {
-            if (typeof obj === "undefined") {
-                console.log("UNDEFINED OBJECT");
-                return;
-            }
-            var index = this.getIndex(bulletClone);
-            if (index != -1 && this.nodes.length) {
-                this.nodes[index].findObjects(returnedObjects, bulletClone);
-            }
-            for (var i = 0, len = objects.length; i < len; i++) {
-                returnedObjects.push(objects[i]);
-            }
-            return returnedObjects;
-        };
-        /*
-         * Insert the object into the quadTree. If the tree
-         * excedes the capacity, it will split and add all
-         * objects to their corresponding nodes.
-         */
-        this.insert = function(obj) {
-            if (typeof bulletClone === "undefined") {
-                return;
-            }
-            if (bulletClone instanceof Array) {
-                for (var i = 0, len = bulletClone.length; i < len; i++) {
-                    this.insert(bulletClone[i]);
-                }
-                return;
-            }
-            if (this.nodes.length) {
-                var index = this.getIndex(bulletClone);
-                // Only add the object to a subnode if it can fit completely
-                // within one
-                if (index != -1) {
-                    this.nodes[index].insert(bulletClone);
-                    return;
-                }
-            }
-            bullets.push(bulletClone);
-            // Prevent infinite splitting
-            if (objects.length > maxObjects && level < maxLevels) {
-                if (this.nodes[0] == null) {
-                    this.split();
-                }
-                var i = 0;
-                while (i < bullets.length) {
-                    var index = this.getIndex(bullets[i]);
-                    if (index != -1) {
-                        this.nodes[index].insert((bullets.splice(i,1))[0]);
-                    }
-                    else {
-                        i++;
-                    }
-                }
-            }
-        };
-        /*
-         * Determine which node the object belongs to. -1 means
-         * object cannot completely fit within a node and is part
-         * of the current node
-         */
-        this.getIndex = function(bulletClone) {
-            var index = -1;
-            var verticalMidpoint = this.bounds.x + this.bounds.width / 2;
-            var horizontalMidpoint = this.bounds.y + this.bounds.height / 2;
-            // Object can fit completely within the top quadrant
-            var topQuadrant = (bulletClone.y < horizontalMidpoint && bulletClone.y + bulletClone.radius < horizontalMidpoint);
-            // Object can fit completely within the bottom quandrant
-            var bottomQuadrant = (bulletClone.y > horizontalMidpoint);
-            // Object can fit completely within the left quadrants
-            if (bulletClone.x < verticalMidpoint &&
-                    bulletClone.x + bulletClone.width < verticalMidpoint) {
-                if (topQuadrant) {
-                    index = 1;
-                }
-                else if (bottomQuadrant) {
-                    index = 2;
-                }
-            }
-            // Object can fix completely within the right quandrants
-            else if (bulletClone.x > verticalMidpoint) {
-                if (topQuadrant) {
-                    index = 0;
-                }
-                else if (bottomQuadrant) {
-                    index = 3;
-                }
-            }
-            return index;
-        };
-        /*
-         * Splits the node into 4 subnodes
-         */
-        this.split = function() {
-            // Bitwise or [html5rocks]
-            var subWidth = (this.bounds.width / 2) | 0;
-            var subHeight = (this.bounds.height / 2) | 0;
-            this.nodes[0] = new QuadTree({
-                x: this.bounds.x + subWidth,
-                y: this.bounds.y,
-                width: subWidth,
-                height: subHeight
-            }, level+1);
-            this.nodes[1] = new QuadTree({
-                x: this.bounds.x,
-                y: this.bounds.y,
-                width: subWidth,
-                height: subHeight
-            }, level+1);
-            this.nodes[2] = new QuadTree({
-                x: this.bounds.x,
-                y: this.bounds.y + subHeight,
-                width: subWidth,
-                height: subHeight
-            }, level+1);
-            this.nodes[3] = new QuadTree({
-                x: this.bounds.x + subWidth,
-                y: this.bounds.y + subHeight,
-                width: subWidth,
-                height: subHeight
-            }, level+1);
-        };
-    }
-    function detectCollision() {
-        game.quadTree.getAllObjects(bullets);
-        for (var x = 0, len = bullets.length; x < len; x++) {
-            game.quadTree.findObjects(obj = [], bullets[x]);
-            for (y = 0, length = bullets.bulletClone[y].length; y < length; y++) {
-                // DETECT COLLISION ALGORITHM
-                if (bullets[x].collidableWith === obj[y].type &&
-                    (bullets[x].x < obj[y].x + obj[y].width &&
-                     bullets[x].x + objects[x].width > obj[y].x &&
-                     bullets[x].y < bullets.bulletClone[y].y + bullets.bulletClone[y].height &&
-                     bullets[x].y + objects[x].height > obj[y].y)) {
-                    bullets[x].isColliding = true;
-                    bullets.bulletClone[y].isColliding = true;
-                }
-            }
+    function getQuadrant(x, y){
+        var quads = document.getElementById('quads');
+        var quadrant;
+        if (x<750 && y<565){
+            quadrant = "q1"
         }
+        if (x>750 && y<565){
+            quadrant = "q2"
+        }
+        if (x<750 && y>565){
+            quadrant = "q3"
+        }
+        if (x>750 && y>565){
+            quadrant = "q4"
+        }
+        quads.textContent = quadrant
     }
     function updateHitbox(){
-        for (i=0; i<bullet.length; i++){
-            var distance = Math.sqrt((bullets[i].x *bullets[i].x) + (bullets[i].y * bullets[i].y));
-            let health = document.getElementById("health")
-            if (distance < bullets[i].radius + RADIUS + 6 && invincible == false) {
-                console.log("whoops");
-                ctx.fillStyle = "red";
-                ctx.fill();
-                health.value -= 20;
-                invincible = true;
-                bullets.splice(i, 1)
+            if (quadrant = "q1"){
+                for (i=0; i<bullets.length; i++){
+                    if (bullets[i].x <= 750 && bullets[i].y <= 565){
+                        var distance = Math.sqrt((bullets[i].x **2) + (bullets[i].y **2));
+                        let health = document.getElementById("health")
+                        if (distance < bullets[i].radius + RADIUS + 6 && invincible == false) {
+                            console.log("whoops");
+                            ctx.fillStyle = "white";
+                            ctx.fill();
+                            health.value -= 20;
+                            invincible = true;
+                            bullets.splice(i, 1)
+                        }
+                    }
+                }
             }
-        }
+            if (quadrant = "q2"){
+                for (i=0; i<bullets.length; i++){
+                    if (bullets[i].x > 750 && bullets[i].y < 565){
+                        var distance = Math.sqrt((bullets[i].x **2) + (bullets[i].y **2));
+                        let health = document.getElementById("health")
+                        if (distance < bullets[i].radius + RADIUS + 6 && invincible == false) {
+                            console.log("whoops");
+                            ctx.fillStyle = "white";
+                            ctx.fill();
+                            health.value -= 20;
+                            invincible = true;
+                            bullets.splice(i, 1)
+                        }
+                    }
+                }
+            }
+            if (quadrant = "q3"){
+                for (i=0; i<bullets.length; i++){
+                    if (bullets[i].x < 750 && bullets[i].y > 565){
+                        var distance = Math.sqrt((bullets[i].x **2) + (bullets[i].y **2));
+                        let health = document.getElementById("health")
+                        if (distance < bullets[i].radius + RADIUS + 6 && invincible == false) {
+                            console.log("whoops");
+                            ctx.fillStyle = "white";
+                            ctx.fill();
+                            health.value -= 20;
+                            invincible = true;
+                            bullets.splice(i, 1)
+                        }
+                    }
+                }
+            }
+            if (quadrant = "q4"){
+                for (i=0; i<bullets.length; i++){
+                    if (bullets[i].x > 750 && bullets[i].y > 565){
+                        var distance = Math.sqrt((bullets[i].x **2) + (bullets[i].y **2));
+                        let health = document.getElementById("health")
+                        if (distance < bullets[i].radius + RADIUS + 6 && invincible == false) {
+                            console.log("whoops");
+                            ctx.fillStyle = "white";
+                            ctx.fill();
+                            health.value -= 20;
+                            invincible = true;
+                            bullets.splice(i, 1)
+                        }
+                    }
+                }
+            }
     }
     if (invincible == true){
         setTimeout(function(){
             invincible = false
-        }, 100)
+        }, 300)
     }
     setTimeout(function(){
         window.requestAnimationFrame(moveBullet);},2500)
@@ -322,7 +217,9 @@ $(document).ready(function() {
                 (bullets[i].xMove =="0" && bullets[i].yMove =="0")||bullets[i].position >= .8||bullets[i].position <= .15||((bullets[i].xMove =="0" || bullets[i].yMove =="0")&& i/2 ===0)){
                 bullets.splice(i, 1);
             }
+
         }
+        updateHitbox();
         window.requestAnimationFrame(moveBullet);
         canvasDraw();
         placeBullets();
@@ -356,11 +253,15 @@ $(document).ready(function() {
         for (i= 0; i<=10; i++){
             makeBullet()
         }
-        console.log(bullets.length)
     }, 100)
     setInterval(function(){
         d= Math.random()/Math.random() *500
         g = Math.random()/Math.random() * 500
     })
-    this.quadTree = new QuadTree({x:0,y:0,width:this.mainCanvas.width,height:this.mainCanvas.height});
+        setInterval(function(){
+            console.log(invincible)
+            if (invincible = true){
+                invincible = false
+            }
+        }, 1000)
 })
