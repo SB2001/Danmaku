c1b7832e8d5ff484d847e8e8b31bdd848de53eca
Sarah Bailey <sabailey@sabailey-mn1.linkedin.biz>
2018-08-07 10:14:11 -0700

    seventh commit

--- danmaku.js
+++ danmaku.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    
 
     const RADIUS = 4;
     const RAD = 4;
@@ -7,12 +8,16 @@ $(document).ready(function() {
       var result = Math.PI / 180 * degrees;
       return result;
     }
+    var levelUp = 0
+    var level = 0
+    var spawnRate = 500;
     var invincible = false
-    var quadrant;
+    var colors = ["#F03C69", "#00fff2", "#fff98c", "#cd9bff", "#6dff68", "#ff8438"]
+    var color = colors[Math.floor(Math.random() * colors.length)]
     var bullets = []
     var canvas = document.querySelector('canvas');
     var ctx = canvas.getContext('2d');
-    var context = canvas.getContext("2d");
+    var bullet = canvas.getContext("2d");
     var x = 740;
     var y = 803;
     var d = 720;
@@ -21,6 +26,7 @@ $(document).ready(function() {
     var frames = 0;
     var directions = ['+', '-','+', '-','+', '-', '0']
     var speeds = [.5, 1, 1.5, 2, 2.5, 3]
+    var colors = []
     function canvasDraw() {
         ctx.fillStyle = "black";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -30,7 +36,10 @@ $(document).ready(function() {
         ctx.fill();
         }
     canvasDraw();
-    placeBullets();
+    alert("Welcome to this topdown shooter demo!\n\nUse your mouse to help the blue dot dodge the other dots\n\nYou have 5 lives, Good luck!\n\nClick on the Canvas to begin")
+    canvas.onclick = function() {
+            canvas.requestPointerLock();
+
     // pointer lock object forking for cross browser
 
     canvas.requestPointerLock = canvas.requestPointerLock ||
@@ -39,12 +48,10 @@ $(document).ready(function() {
     document.exitPointerLock = document.exitPointerLock ||
                                document.mozExitPointerLock;
 
-    canvas.onclick = function() {
-      canvas.requestPointerLock();
-    };
+    
+    
 
     // pointer lock event listeners
-
     // Hook pointer lock state change events for different browsers
     document.addEventListener('pointerlockchange', lockChangeAlert, false);
     document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
@@ -93,24 +100,26 @@ $(document).ready(function() {
         });
       }
     }
-
     function updateHitbox(){
+        let health = document.getElementById("health")
                 for (i=0; i<bullets.length; i++){
-                        var distance = Math.sqrt(((bullets[i].x **2) + (bullets[i].y **2))-((player.x **2)+(player.y**2)));
-                        let health = document.getElementById("health")
-                        if (distance < bullets[i].radius + RADIUS + 6 && invincible == false) {
-                            console.log(player.x, player.y, bullets[i].x, bullets[i].y);
-                            ctx.fillStyle = "white";
-                            ctx.fill();
+                        var distance = Math.sqrt( Math.pow((bullets[i].x - player.x), 2) + Math.pow((bullets[i].y - player.y),2) );
+                        // Math.sqrt(((bullets[i].x **2) + (bullets[i].y **2))-((player.x **2)+(player.y**2)));
+                        if (distance < bullets[i].radius + player.radius && invincible == false ) {
                             health.value -= 20;
                             invincible = true;
+                            bullets.splice(i, 1)
+                        if(health.value == 0){
+                            alert("Game Over\n\nScore:" + frames + "\n\n Level: " +levelUp);
+                            window.location.reload();
+                        }
                     }
                 }
             }
     if (invincible == true){
         setTimeout(function(){
             invincible = false
-        }, 300)
+        }, 3000)
     }
     setTimeout(function(){
         window.requestAnimationFrame(moveBullet);},2500)
@@ -121,7 +130,7 @@ $(document).ready(function() {
     function moveBullet(){
         placeBullets();
         for(i = 0; i<bullets.length; i++){
-            context.clearRect(0, 0, canvas.width, canvas.height)
+            bullet.clearRect(0, 0, canvas.width, canvas.height)
             if (bullets[i].xMove == "+"){
                 bullets[i].x += bullets[i].position * bullets[i].xSpeed;
             }
@@ -142,7 +151,7 @@ $(document).ready(function() {
                 bullets[i].y += 0
                 bullets[i].x *.5
             }
-            if (bullets[i].x <= 0 || bullets[i].x >= 1500 || bullets[i].y <= 0 || bullets[i].y >= 1125 ||
+            if (bullets[i].x <= -100 || bullets[i].x >= 2000 || bullets[i].y <= -100 || bullets[i].y >= 1200 ||
                 (bullets[i].xMove =="0" && bullets[i].yMove =="0")||bullets[i].position >= .8||bullets[i].position <= .15||((bullets[i].xMove =="0" || bullets[i].yMove =="0")&& i/2 ===0)){
                 bullets.splice(i, 1);
             }
@@ -153,14 +162,19 @@ $(document).ready(function() {
         placeBullets();
         updateHitbox();
         frames = frames + 1
+        level = level + 1
+        $("#score").html('Score = '+ frames + " Level: " + levelUp)
+        if (level === 625){
+            levelUp += 1
+            level = 0
+        }
     }
-    $("#score").html('Score = '+ frames)
     function placeBullets(){
         for(i = 0; i<bullets.length; i++){
-            context.beginPath();
-            context.arc(bullets[i].x, bullets[i].y, bullets[i].radius, 0, 2 * Math.PI, false)
-            context.fillStyle = "#F03C69"
-            context.fill();
+            bullet.beginPath();
+            bullet.arc(bullets[i].x, bullets[i].y, bullets[i].radius, 0, 2 * Math.PI, false)
+            bullet.fillStyle = color
+            bullet.fill();
         }
     }
     function makeBullet(){
@@ -179,10 +193,75 @@ $(document).ready(function() {
         bullets.push(bulletClone);
     }
     setInterval(function(){
-        for (i= 0; i<=5; i++){
-            makeBullet()
+        if (levelUp == 0) {
+            for (i= 0; i<=5; i++){
+                makeBullet()
+            }
+        }
+    }, 400)
+    setInterval(function(){
+        if (levelUp == 1) {
+            for (i= 0; i<=5; i++){
+                makeBullet()
+            }
+        }
+    }, 350)
+    setInterval(function(){
+        if (levelUp == 2) {
+            for (i= 0; i<=5; i++){
+                makeBullet()
+            }
+        }
+    }, 300)
+    setInterval(function(){
+        if (levelUp == 3) {
+            for (i= 0; i<=5; i++){
+                makeBullet()
+            }
+        }
+    }, 250)
+    setInterval(function(){
+        if (levelUp == 4) {
+            for (i= 0; i<=5; i++){
+                makeBullet()
+            }
         }
     }, 200)
+    setInterval(function(){
+        if (levelUp == 5) {
+            for (i= 0; i<=5; i++){
+                makeBullet()
+            }
+        }
+    }, 150)
+    setInterval(function(){
+        if (levelUp == 6) {
+            for (i= 0; i<=5; i++){
+                makeBullet()
+            }
+        }
+    }, 100)
+    setInterval(function(){
+        if (levelUp == 7) {
+            for (i= 0; i<=5; i++){
+                makeBullet()
+            }
+        }
+    }, 50)
+    setInterval(function(){
+        if (levelUp >= 8 && levelUp < 11) {
+            for (i= 0; i<=5; i++){
+                makeBullet()
+            }
+        }
+    }, 5)
+    setInterval(function(){
+        if (levelUp == 11) {
+                bullets=[]
+                alert("Congratulations")
+                window.location.reload();
+        }
+    }, 50)
     setInterval(function(){
         d= Math.random()/Math.random() *500
         g = Math.random()/Math.random() * 500
@@ -192,5 +271,5 @@ $(document).ready(function() {
             if (invincible = true){
                 invincible = false
             }
-        }, 1000)
-})
+        }, 3000)
+}})
